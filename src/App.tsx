1ef7046b521d3b04b5387455ac344702363e6087
Leonard Tng <leonardtng@gmail.com>
2020-01-10 18:31:27 +0800

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './logo.34f82a1a.svg';
 import './App.css';
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="row">
+        <div className="col-md-4">
+        </div>
+
+        <div className="col-md-4 block">
+          <header>
+            <div className="img-logo">
+              <img src={logo} alt="icon"/>
+            </div>                    
+            <div className="student-portal">
+              <span>Student Portal</span>
+            </div>                   
+          </header>
+
+          <form>
+            <div id="user">
+              <div className="form-group">
+                <label>Username</label>
+                  <input type="username" className="form-control" id="username"/>
+                </div>
+              <div className="form-group">
+                <label>Password</label>
+                <input type="password" className="form-control" id="password"/>
+                <div className='incorrect'>{''}</div>  
+              </div> 
+            </div>
+            <div className="text-center">
+              <button type="button" className="btn btn-primary">Login</button>                        
+            </div>                    
+          </form>
+          </div>
+
+          <div className="col-md-4">
+          </div>
+      </div>
     </div>
   );
 }
